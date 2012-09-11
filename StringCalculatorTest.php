67f4ab46259e40c6b14645cb786d058795ed3133
matason <chris.maiden@ymail.com>
2012-09-11 22:36:43 +0100

    Initial commit, starting point for String Calculator Kata.

---
+++ StringCalculatorTest.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'StringCalculator.php';
+
+class StringCalculatorTest extends PHPUnit_Framework_TestCase {
+  protected $sc;
+
+  protected function setUp() {
+    $this->sc = new StringCalculator;
+  }
+
+  public function testEmptyStringReturnsZero() {
+    $this->assertSame(0, $this->sc->add(''), 'An empty string returns zero.'); 
+  }
+}
