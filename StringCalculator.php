540b32326bd02667ebed6e554532225be05231aa
matason <chris.maiden@ymail.com>
2012-11-24 05:53:51 +0000

    Cleared test step comments, start with minimal code.

--- StringCalculator.php
+++ StringCalculator.php
@@ -3,12 +3,8 @@
 /**
  * @file
  * String calculator class definition.
- *
- * @see http://nimblepros.com/media/36628/string%20calculator%20kata.pdf
  */
 
 class StringCalculator {
-  public function add() {
-  	
-  }
+  public function add() {}
 }
--- StringCalculatorTest.php
+++ StringCalculatorTest.php
@@ -5,43 +5,6 @@
  * String calculator tests.
  */
 
-// Includes the file you would like to perform tests on
-require_once 'StringCalculator.php';
+require_once 'StringCalculatorFrameworkTestCase.php';
 
-class StringCalculatorTest extends PHPUnit_Framework_TestCase {
-  protected $sc;
-  protected function setUp() {
-    $this->sc = new StringCalculator;
-  }
-  /**
-   * String calculator tests.
-   */
-  protected function stringAdd($string) {
-  	return $this->sc->add($string);
-  }
-  // 1. Will an empty string ('') return 0?
-  // Tests are public methods that are named test*. 
-  public function testEmptyStringReturnsZero() {
-  	// $this->assertSame(mixed $expected, mixed $actual[, string $message = '']);
-    $this->assertSame(0, $this->stringAdd(''), 'An empty string returns zero.');
-  }
-  
-  // 2. Will 1 return 1?
-  //public function testSingleInputReturnsItself() {
-    //$this->assertSame(1, $this->stringAdd('1'), 'An empty string returns zero.');
-  //}
-
-  // 3. Will 1+2 return 3?
-  //public function testInputsReturnsSum() {
-    //$this->assertSame(3, $this->stringAdd('0,1,2'), 'An empty string returns zero.');
-  //}
-
-  // 4. We dont want numbers too large, will outputs over 1000 return 'I cant count that high!'?
-
-  // 5. Will an unknown number of inputs such as '1,1,3,5,10' return their sum?
-
-  // 6. Will mixed delimiters such as 1\n2,3 still return as sum?
-
-  // 7. Will negative numbers be dealt with correctly?
-
-}
+class StringCalculatorTest extends StringCalculatorFrameworkTestCase {}
